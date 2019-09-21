787ba8e0626136d2db3cd64fd7301bb63448eda5
cassianodev1 <55609525+cassianodev1@users.noreply.github.com>
2019-09-21 20:12:00 -0300

    Adição do sistema da loja

---
+++ MarquesAnimation/Controle/ControleLoja.cs
@@ -0,0 +1,33 @@
+﻿using MarquesAnimation.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarquesAnimation.Controle
+{
+    class ControleLoja
+    {
+        public string AdicionarLoja(Loja loja)
+        {
+            LojaDAO lj = new LojaDAO();
+            string mensagem = lj.Adicionar(loja);
+            return mensagem;
+        }
+
+        public SqlDataReader RetornarLoja()
+        {
+            LojaDAO lj = new LojaDAO();
+            return lj.RetornaLoja();
+        }
+
+        public string DeletarLoja(int indice)
+        {
+            LojaDAO lj = new LojaDAO();
+            string mensagem = lj.Deletar(indice);
+            return mensagem;
+        }
+    }
+}
