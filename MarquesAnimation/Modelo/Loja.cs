9684e5f7f0a3f7dbbeffa3f13ef5c1a3a1c2a842
cassianodev1 <55609525+cassianodev1@users.noreply.github.com>
2019-09-21 20:12:49 -0300

    Adição do sistema da loja

---
+++ MarquesAnimation/Modelo/Loja.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarquesAnimation.Modelo
+{
+    class Loja
+    {
+        public int ID { get; set; }
+        public string CPFF { get; set; }
+        public string NomeP { get; set; }
+        public string CNPJD { get; set; }
+
+        public Loja(int ID, string CPFF, string NomeP, string CNPJD)
+        {
+            this.ID = ID;
+            this.CPFF = CPFF;
+            this.NomeP = NomeP;
+            this.CNPJD = CNPJD;
+        }
+    }
+}
